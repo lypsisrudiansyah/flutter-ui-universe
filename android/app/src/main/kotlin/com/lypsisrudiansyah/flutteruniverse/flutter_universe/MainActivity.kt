47f3a18fbdcca5c2573fafd9a7cdfb966f4835ed
rudiansyah <rudiansyahether@gmail.com>
2020-11-18 13:24:28 +0700

    initial commit
    fresh project

---
+++ android/app/src/main/kotlin/com/lypsisrudiansyah/flutteruniverse/flutter_universe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lypsisrudiansyah.flutteruniverse.flutter_universe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_universe/screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Universe',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
